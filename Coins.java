e1040ab3978528960276392d69a2565360549f22
idan <idanlips@gmail.com>
2023-12-20 15:19:25 +0200

    small changes

--- Coins.java
+++ Coins.java
@@ -2,7 +2,10 @@ public class Coins {
 public static void main (String[]args){
 	int a = Integer.parseInt(args[0]);
 	int quarters = a/25; 
-	int cents = a%(quarters*25);
+	int cents = a%25;
+	if (a <= 0){
+		System.out.println("you dont have any money");
+	}
 		System.out.println("Use " + quarters + " quarters " + "and " + cents +      	 " cents"); 
 
 	} 
