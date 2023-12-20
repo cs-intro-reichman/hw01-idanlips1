179f61fdcd12f5b886e80a2e43aa53498a306dfc
idan <idanlips@gmail.com>
2023-12-20 15:24:14 +0200

    fixed

--- LinearEq.java
+++ LinearEq.java
@@ -3,6 +3,10 @@ public static void main (String[]args){
 	double a = Double.parseDouble(args[0]);
 	double b = Double.parseDouble(args[1]);
 	double c = Double.parseDouble(args[2]);
+	if (a == 0) {
+		System.out.println("The coefficient 'a' cannot be zero for a linear equation.");
+		return;
+	}
 	double x = (c-b)/a;	
 		System.out.println(a + " * " + "x " + "+ " + b + " = " + c);
 		System.out.println("X " + "= " + x); 
